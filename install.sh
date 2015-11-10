1f36b047f180f52d1348b335d0556024530d7ece
Eirikur Ari Petursson <eirikur.ari@hotmail.com>
2015-11-10 20:36:47 +0000

    some backup function

--- install.sh
+++ install.sh
@@ -1,38 +1,57 @@
 #!/usr/bin/env bash
 
-# Let's get the full path where the script and git repository resides
+# Let's get the full path where the script and git repository are located
 my_dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 my_git=" git --work-tree="$my_dotfiles" --git-dir="$my_dotfiles/.git" ";
 
 # List of dotfiles that we want to link to users home directory
 declare -a listOfFiles=(".gitignore_global" ".bashrc" ".bash_profile" ".bash_prompt" ".aliases" ".functions" ".exports" ".inputrc");
 
-# Create symlinks for my dotfiles
-function linkIt() {
+# Update my git repo and submodules
+function gitMe() {
+  [ -d "$my_dotfiles/.git" ] && ${my_git} pull origin master && ${my_git} submodule update;
+}
+
+# Move dotfiles to backup directory
+function doBackup() {
+  #create backup folder if it does not exist
+  local backup_dir="dotfile_backup";
+  if [ -e ~/$backup_dir]; then
+    mkdir -pv ~/$backup_dir;
+  fi
 
   for dotfile in "${listOfFiles[@]}"
-	do
-		#  dotfile exists and is not a symlink 
-	  if [[ -e ~/$dotfile && ! -L ~/$dotfile ]]; then
-      # lets create a backup of existing file and replace it with a symlink 
-			mv -v ~/$dotfile ~/$dotfile.bak;
-			# create the symlink
-			ln -sfv "$my_dotfiles/$dotfile" ~;
-		elif [[ ! -e ~/$dotfile && ! -L ~/$dotfile ]]; then 
-			# if dotfile is not a symlink, then create the symlink
-		  ln -sfv "$my_dotfiles/$dotfile" ~;
-		fi
-	done
+  do
+    if [[ -e ~/$dotfile && ! -L ~/$dotfile ]]; then
+      # lets create a backup of existing file and replace it with a symlink
+      mv -v ~/$dotfile ~/$dotfile.bak;
+      ln -sfv "$my_dotfiles/$dotfile" ~;
+    fi
+  done
+}
 
+# Create symlinks for my dotfiles
+function linkIt() {
+  for dotfile in "${listOfFiles[@]}"
+  do
+    if [[ -e ~/$dotfile && ! -L ~/$dotfile ]]; then
+      # lets create a backup of existing file and replace it with a symlink
+	  mv -v ~/$dotfile ~/$dotfile.bak;
+	  ln -sfv "$my_dotfiles/$dotfile" ~;
+    elif [[ ! -e ~/$dotfile && ! -L ~/$dotfile ]]; then 
+	  # create the symlink
+	  ln -sfv "$my_dotfiles/$dotfile" ~;
+	fi
+  done
 }
 
 
 # Make sure we have the latest update
-[ -d "$my_dotfiles/.git" ] && ${my_git} pull origin master && ${my_git} submodule update;
+gitMe;
 
 # install & activate
 linkIt;
 source ~/.bashrc;
 
 # Cleanup
-unset my_dotfiles my_git listOfFiles linkIt;
+unset my_dotfiles my_git listOfFiles gitMe doBackup linkIt;
