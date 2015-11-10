ea6ae4c65079c678e2b837f0a09cd8e0b0f2a929
Eirikur Ari Petursson <eirikur.ari@hotmail.com>
2015-11-10 14:59:10 +0000

    Creates symlinks, backup existing files (non symlinked) and cleansup

--- install.sh
+++ install.sh
@@ -3,13 +3,31 @@
 # Let's get the full path where the script resides
 my_dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Create symlinks for my dotfiles
-ln -sfv "$my_dotfiles/.bashrc" ~
-ln -sfv "$my_dotfiles/.bash_profile" ~
-ln -sfv "$my_dotfiles/.bash_prompt" ~
-ln -sfv "$my_dotfiles/.aliases" ~
-ln -sfv "$my_dotfiles/.functions" ~
-ln -sfv "$my_dotfiles/.exports" ~
-ln -sfv "$my_dotfiles/.inputrc" ~
+# List of dotfiles that we want to link to users home directory
+declare -a listOfFiles=(".bashrc" ".bash_profile" ".bash_prompt" ".aliases" ".functions" ".exports" ".inputrc")
 
+# Create symlinks for my dotfiles
+function linkIt() {
+
+  for dotfile in "${listOfFiles[@]}"
+	do
+
+		#  dotfile exists and is not a symlink 
+	  if [[ -e ~/$dotfile && ! -L ~/$dotfile ]]; then
+      # lets create a backup of existing file
+			mv -v ~/$dotfile ~/$dotfile.bak
+		fi
+
+		# create symlink
+		ln -sfv "$my_dotfiles/$dotfile" ~
+
+	done
+
+}
+
+# install
+linkIt
 source ~/.bashrc
+
+# Cleanup
+unset my_dotfiles listOfFiles
