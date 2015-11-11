3b4579a3421a215055457860607712d036a941a4
Eirikur Ari Petursson <eirikur.ari@hotmail.com>
2015-11-11 18:18:14 +0000

    Reorganized the structure and updated the install script according to that change

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ my_dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 my_git=" git --work-tree="$my_dotfiles" --git-dir="$my_dotfiles/.git" ";
 
 # List of dotfiles that we want to link to users home directory
-declare -a listOfFiles=(".gitignore_global" ".bashrc" ".bash_profile" ".bash_prompt" ".aliases" ".functions" ".exports" ".inputrc");
+declare -a listOfFiles=("git/.gitignore_global" "git/.gitconfig" "bash/.bashrc" "bash/.bash_profile" "bash/.bash_prompt" "others/.aliases" "others/.functions" "others/.exports" "others/.inputrc");
 
 # Update my git repo and submodules
 function gitMe() {
@@ -22,20 +22,19 @@ function doBackup() {
     
   # lets create a backup of existing file and replace it with a symlink
   mv -v ~/$dotfile ~/$backup_dir/$dotfile;
-  ln -sfv "$my_dotfiles/$dotfile" ~;
 }
 
 # Create symlinks for my dotfiles
 function linkIt() {
   for dotfile in "${listOfFiles[@]}"
   do
-  	if [[ -e ~/$dotfile && ! -L ~/$dotfile ]]; then
+    if [[ -e ~/$dotfile && ! -L ~/$dotfile ]]; then
       # We might want to backup our stuff
       doBackup $dotfile;
-    elif [[ ! -e ~/$dotfile && ! -L ~/$dotfile ]]; then 
-	  # create the symlink
-	  ln -sfv "$my_dotfiles/$dotfile" ~;
-	fi
+    fi
+    
+    # create the symlink
+    ln -sfv "$my_dotfiles/$dotfile" ~;
   done
 }
 
