888c06173bbca333c72d4916c8c75b38ed53a2ac
Eirikur Ari Petursson <eirikur.ari@hotmail.com>
2026-04-12 16:38:20 +0000

    updated plugins, added telescope setup, changed leader key for nvim

--- tools/nvim/lua/config/lazy.lua
+++ tools/nvim/lua/config/lazy.lua
@@ -19,9 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -36,7 +34,7 @@ require("lazy").setup({
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
-    enabled = true, -- check for plugin updates periodically
+    enabled = false, -- check for plugin updates periodically
     notify = false, -- notify on update
   }, -- automatically check for plugin updates
   performance = {
--- tools/nvim/lua/config/options.lua
+++ tools/nvim/lua/config/options.lua
@@ -1,4 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set the leader key
+vim.g.mapleader = " " -- space is the leader
+vim.g.maplocalleader = " "
+
+-- Do not use releative number
 vim.opt.relativenumber = false
